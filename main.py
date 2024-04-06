10734879409d6b2c1aaf3625671e7e1bf34dd9a9
Daniel Brandao <danielbrandao@lampp-it.com.br>
2024-04-06 16:59:00 -0300

    up main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from config import SQLALCHEMY_DATABASE_URI, mongodb
+from config import SQLALCHEMY_DATABASE_URI, mongodb, pedidos_collection
 from flask import Flask, jsonify, request
 from sqlalchemy import Integer, String, Float, Date
 from flask_sqlalchemy import SQLAlchemy
@@ -40,6 +40,20 @@ class Clientes(mysql.Model):
             "data_nascimento": self.data_nascimento
         }
 
+class Pedido():
+    def __init__(self, id_cliente, id_produto, data_pedido, valor_pedido):
+        self.id_cliente = id_cliente
+        self.id_produto = id_produto
+        self.data_pedido = data_pedido
+        self.valor_pedido = valor_pedido
+
+    def serialize(self):
+        return {
+            "id_cliente": self.id_cliente,
+            "id_produto": self.id_produto,
+            "data_pedido": self.data_pedido,
+            "valor_pedido": self.valor_pedido,
+        }
 @app.route("/", methods=['GET'])
 def index():
     return '<h1>Olá, mundo.</h1>'
@@ -99,6 +113,39 @@ def delete_produto(id):
 def get_clientes():
     clientes = Clientes.query.all()
     return jsonify([cliente.serialize() for cliente in clientes])
+
+@app.route("/pedidos", methods=['POST'])
+def set_pedido():
+    dados = request.get_json()
+    novo_pedido = Pedido(
+        id_produto=dados["id_produto"],
+        id_cliente=dados['id_cliente'],
+        data_pedido=dados["data_pedido"],
+        valor_pedido=dados["valor_pedido"]
+    )
+    resultado = pedidos_collection.insert_one(novo_pedido.serialize())
+    if resultado.inserted_id:
+        # Retorna o pedido recém-criado e o status 201
+        novo_pedido.id_pedido = str(resultado.inserted_id)
+        return jsonify(novo_pedido.serialize()), 201
+    else:
+        return "Erro ao inserir pedido.", 500
+
+@app.route("/pedidos", methods=["GET"])
+def get_pedidos():
+    try:
+        pedidos = pedidos_collection.find()
+
+        # Convertendo ObjectId em strings para serialização
+        pedidos_serializaveis = []
+        for pedido in pedidos:
+            pedido['_id'] = str(pedido['_id'])
+            pedidos_serializaveis.append(pedido)
+
+        return jsonify(pedidos_serializaveis), 200
+    except Exception as e:
+        print(f"Erro: {e}")
+        return "Erro ao listar pedidos.", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
